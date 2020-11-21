2cb757d79d213a19fefc691ee63a3bc716f1625b
kudi_eyram <noraeyram.kudiabor@gmail.com>
2020-11-21 17:58:44 +0000

    Editted New Post Component

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -10,18 +10,19 @@ function NewPostComponent() {
   return (
     <div className="login-wrapper" >
         <div className="login-form">
-        <form>
-        <h2 className="text-center">Welcome Kudi_yram Blog!</h2>       
-        <div className="form-group">
-            <input type="text" className="form-control" placeholder="Title of Blog Post" required="required" name="title"/>
-        </div>
-        <div className="form-group">
-            <textarea rows="8" type="text" className="form-control" placeholder="Content of Post" required="required" name="content"/>
-        </div>
-        <div className="form-group">
-            <button className="btn btn-primary btn-block">Add New Blog Post</button>
-        </div>
-              
+          <form>
+            <h2 className="text-center">Welcome Kudi_yram Blog!</h2>       
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Title of Blog Post" required="required" name="title"/>
+            </div>
+
+            <div className="form-group">
+              <textarea rows="8" type="text" className="form-control" placeholder="Content of Post" required="required" name="content"/>
+            </div>
+
+            <div className="form-group">
+              <button className="btn btn-primary btn-block">Add New Blog Post</button>
+            </div>s      
     </form>
         </div>
          
